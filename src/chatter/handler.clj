6215962d224239aef6b70a46daec807b21bbb7d1
Laney <mw@Bills-MacBook-Pro.local>
2015-09-12 15:04:06 -0500

    trying to add all the files to make this go online

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -3,20 +3,36 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as page]
-            [hiccup.form :as form]))
-            
+            [hiccup.form :as form]
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
+
 (def chat-messages (atom '()))
-					
+
+(defn init []
+    (printIn "chatter is starting"))
+
+(defn destroy []
+  (printIn "chatter is shutting"))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
+
 (defn generate-message-view
 	"This generates the HTML for displaying messages"
 	[messages]
 	(page/html5
-		[:head
-			[:title "chatter"]]
+		   [:head
+    [:title "chatter"]
+    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+    (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+    (page/include-css "/chatter.css")]
 		[:body
 			[:h1 "Our Chat App"]
-			[:p 
+			[:p
 				(form/form-to
 					[:post "/"]
 					"Name: " (form/text-field "name")
@@ -30,15 +46,16 @@
 	"This will update a message list atom"
 	[messages name message]
 	(swap! messages conj {:name name :message message}))
-	
+
 (defroutes app-routes
   (GET "/" [] (generate-message-view @chat-messages))
-  (POST "/" {params :params} 
+  (POST "/" {params :params}
   	(let [name-param (get params "name")
   		msg-param (get params "msg")
   			new-messages (update-messages! chat-messages name-param msg-param)]
   		(generate-message-view new-messages)
 		))
+  (route/resources "/")
   (route/not-found "Not Found"))
-  
+
 (def app (wrap-params app-routes))
